f4fb8f8ee9d37532e3e88b81d37faa1f47db263c
Pataki István <patakiistvan1997y@gmail.com>
2023-11-03 19:11:03 +0100

    final touches

--- app/_components/secions/hero/HeroShirt.jsx
+++ app/_components/secions/hero/HeroShirt.jsx
@@ -8,17 +8,52 @@ import { elementIsVisibleInViewport } from '@/app/_utils/utils';
 
 
 const HeroShirt = () => {
+  const colorSet = [
+    [206, 10, 0.4],
+    [228, 5, 0.4],
+    [360, 5, 0.4],
+    [58, 5, 0.6]
+  ];
+
+  const initialColor = {
+    hue: colorSet[0][0],
+    sat: colorSet[0][1],
+    br: colorSet[0][2]
+  }
+
+  const colorRange = 360 - colorSet[0][0] + colorSet[colorSet.length - 1][0];
 
   const shirtImageRef = useRef(null);
   const mount = useRef(true);
   const [activateMouseMove, setActivateMouseMove] = useState(false)
+  const [shirtColor, setShirtColor] = useState(initialColor);
 
-  const calculateShirtColor = function (event) {
-    console.log(event.clientX / window.innerWidth * 100)
-  }
 
-  const mousemoveHandler = useCallback(() => {
-    console.log("MOUSE MOVED")
+  const mousemoveHandler = useCallback((e) => {
+
+
+    const mouseXPercent = e.clientX / window.innerWidth;
+    const hueExtra = colorRange * mouseXPercent;
+    const placeInHue = (colorRange + hueExtra) % 360;
+
+    for (let i = 1; i < colorSet.length; i++) {
+      const setI = colorSet[i]
+      if (placeInHue >= colorSet[i - 1][0] % 360 && placeInHue <= setI[0]) {
+        const setPrev = colorSet[i - 1];
+        const placeBetween = (placeInHue - setPrev[0] % 360) / (setI[0] - setPrev[0] % 360);
+        const setHue = setPrev[0] % 360 + (setI[0] - setPrev[0] % 360) * placeBetween;
+        const setSat = setPrev[1] + (setI[1] - setPrev[1]) * placeBetween;
+        const setBr = setPrev[2] + (setI[2] - setPrev[2]) * placeBetween;
+        setShirtColor({
+          hue: setHue,
+          sat: setSat,
+          br: setBr
+        })
+        break
+      }
+    }
+
+
   }, []);
 
   useEffect(() => {
@@ -35,7 +70,6 @@ const HeroShirt = () => {
 
   const scrollFunction = function () {
     const isVisible = shirtImageRef.current ? elementIsVisibleInViewport(shirtImageRef.current, true) : false;
-    // if (isVisible) { console.log("scrolled and VISIBLE") } else { console.log("scrolled and invisible") }
     if (isVisible !== activateMouseMove) {
       setActivateMouseMove(isVisible)
     }
@@ -45,7 +79,12 @@ const HeroShirt = () => {
   return (
     <>
       {/* <Image className="img__shirt" style={{ filter: `hue-rotate(${String(shirtColor.hue)}deg) saturate(${String(shirtColor.sat)}) brightness(${String(shirtColor.br)})` }} src="/assets/images/ruha.png" alt="A pic of of the human's shirt. It looks cool." /> */}
-      <Image ref={shirtImageRef} className="img__shirt" src={shirtImage} alt="A pic of of the human's shirt. It looks cool." />
+      <Image
+        ref={shirtImageRef}
+        className="img__shirt" src={shirtImage}
+        alt="Color changing shirt"
+        style={{ filter: `hue-rotate(${String(shirtColor.hue)}deg) saturate(${String(shirtColor.sat)}) brightness(${String(shirtColor.br)})` }}
+      />
     </>
   )
 }
--- app/_components/secions/navbar/NavbarClient.jsx
+++ app/_components/secions/navbar/NavbarClient.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React, { useState } from 'react'
 import './navbar.scss'
+import Image from 'next/image'
 
 function NavbarClient(props) {
   const [navMenu, setNavMenu] = useState(false)
@@ -23,7 +24,10 @@ function TopNavbar(props) {
     <header className="navbar">
       {props.logo}
       {props.navItems}
-      <div onClick={props.toggleNav} className="menuIcon"><iconify-icon icon="uil:bars" width="25" height="25"></iconify-icon></div>
+      <div onClick={props.toggleNav} className="menuIcon">
+        {/* <iconify-icon icon="uil:bars" width="25" height="25"></iconify-icon> */}
+        <Image width={25} height={25} src={'data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="25" height="25" viewBox="0 0 24 24"%3E%3Cg id="feBar0" fill="none" fill-rule="evenodd" stroke="none" stroke-width="1"%3E%3Cg id="feBar1" fill="white"%3E%3Cpath id="feBar2" d="M3 16h18v2H3v-2Zm0-5h18v2H3v-2Zm0-5h18v2H3V6Z"%2F%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E'} />
+      </div>
     </header>
   )
 }
@@ -32,9 +36,8 @@ function Slidebar(props) {
 
   return (
     <div className={props.navMenu ? 'isOpen slidebar' : 'isClose slidebar'} onClick={props.toggleNav} >
-      <iconify-icon onClick={props.toggleNav} className="menuicon" icon="uil:times" />
+      <Image className="menuicon" width={32} height={32} src={'data:image/svg+xml,%3Csvg xmlns="http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg" width="25" height="25" viewBox="0 0 24 24"%3E%3Cpath fill="white" d="m13.41 12l4.3-4.29a1 1 0 1 0-1.42-1.42L12 10.59l-4.29-4.3a1 1 0 0 0-1.42 1.42l4.3 4.29l-4.3 4.29a1 1 0 0 0 0 1.42a1 1 0 0 0 1.42 0l4.29-4.3l4.29 4.3a1 1 0 0 0 1.42 0a1 1 0 0 0 0-1.42Z"%2F%3E%3C%2Fsvg%3E'} />
       {props.navItems}
-      <iconify-icon className="menuicon" icon="uil:bars" />
     </div >
   )
 }
